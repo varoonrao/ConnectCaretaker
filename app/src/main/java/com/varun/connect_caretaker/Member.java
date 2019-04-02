58ca184299dc039891a1c65758c6358b3cb4d5c0
varoonrao <varunrao72@gmail.com>
2019-04-02 23:03:14 +0530

    25/03/2019

---
+++ app/src/main/java/com/varun/connect_caretaker/Member.java
@@ -0,0 +1,25 @@
+package com.varun.connect_caretaker;
+
+public class Member {
+    private String userid;
+    private String username;
+
+    public Member() {
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
